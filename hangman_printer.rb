5288cb9be980df8280a4e73feff03016a672cfe9
philiptang42 <philiptang42@gmail.com>
2015-12-16 11:18:53 -0500

    solution (without spaces though)

--- hangman_printer.rb
+++ hangman_printer.rb
@@ -1,3 +1,17 @@
-def print_puzzle(word)
+def print_puzzle(word, guesses = [])
+  underscore = "_" * word.length
+  if word.include?(" ")
+    space = word.index(" ")
+    underscore[space] = " "
+  end
 
+  guesses.each do |guess|
+    if word.include?(guess)
+      space = word.index(guess)
+      underscore[space] = guess
+    end
+  end
+  underscore
 end
+
+puts print_puzzle("triangle", ['t', 's', 'g'])
